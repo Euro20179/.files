847adfc0af0bad96ee06cb5e33b51a8a9dd58a3b
euro20179 <sandollar2020@gmail.com>
2023-10-10 09:03:36 -0700

    utilize after/ftplugin

--- .config/nvim/lua/autocmd.lua
+++ .config/nvim/lua/autocmd.lua
@@ -1,13 +1,3 @@
-vim.api.nvim_create_autocmd("BufReadPost,BufNewFilePost", {
-    pattern = "*.norg",
-    callback = function()
-        vim.api.nvim_cmd({
-            cmd = "norm",
-            args = { "zR" }
-        }, {})
-    end
-})
-
 vim.api.nvim_create_autocmd("TextYankPost", {
     callback = function()
         vim.highlight.on_yank({ timeout = 200, higroup = "RedrawDebugNormal" })
