d8cb87424d63bdb968b6d0cd3b47df4bdb6b5550
euro20179 <sandollar2020@gmail.com>
2023-10-23 09:43:19 -0700

    remove rust stuff

--- .config/nvim/lua/user/dap.lua
+++ .config/nvim/lua/user/dap.lua
@@ -3,40 +3,6 @@ local dap = require "dap"
 
 local HOME = vim.fn.getenv("HOME")
 
--- dap.adapters.rust = {
---     type = 'executable',
---     attach = { pidProperty = "pid", pidSelect = "ask" },
---     command = "codelldb",
---     env = {LLDB_LAUNCH_FLAG_LAUNCH_IN_TTY = "YES"},
---     name = "lldb"
--- }
-
---dap.adapters.rust = dap.adapters.codelldb
-
--- dap.configurations.rust = {
---     {
---         name = "Rust debug",
---         type = "rust",
---         request = "launch",
---         program = function()
---             local metadata_json = vim.fn.system "cargo metadata --format-version 1 --no-deps"
---             local metadata = vim.fn.json_decode(metadata_json)
---             local target_name = metadata.packages[1].targets[1].name
---             local target_dir = metadata.target_directory
---             return target_dir .. "/debug/" .. target_name
---         end,
---         args = function()
---             local inputstr = vim.fn.input("Params: ", "")
---             local params = {}
---             local sep = "%s"
---             for str in string.gmatch(inputstr, "([^" .. sep .. "]+)") do
---                 table.insert(params, str)
---             end
---             return params
---         end
---     },
--- }
-
 dap.adapters.python = {
     type = "executable",
     command = "/usr/bin/python",
