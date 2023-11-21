4838b066ffa36eeef113bf556a69d22b66e85fae
euro20179 <sandollar2020@gmail.com>
2023-11-21 15:58:23 -0800

    update

--- .config/nvim/lua/colorscheme.lua
+++ .config/nvim/lua/colorscheme.lua
@@ -72,6 +72,7 @@ _M.changeColorScheme = function(newScheme)
         cmd = "colorscheme"
     }, {})
     vim.cmd[[hi Normal guibg=#00000000]]
+    vim.cmd[[hi Winbar guibg=#00000000]]
     vim.api.nvim_set_hl(0, "LspInlayHint", {bg = "NONE", italic = true, fg="#6e738d"})
     vim.api.nvim_set_hl(0, "CmpItemAbbrDeprecated", { bg = "NONE", strikethrough = true})
 end
