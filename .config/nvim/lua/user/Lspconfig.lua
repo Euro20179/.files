c09b8690da54fc72bf0f706329ce051ebf55e8d9
euro20179 <sandollar2020@gmail.com>
2024-06-13 18:28:34 -0700

    config:fix(hyprland): cursor broke scripts(linkhandler): handle folders and pdfs scripts(linkhandler): default to $BROWSER

--- .config/nvim/lua/user/Lspconfig.lua
+++ .config/nvim/lua/user/Lspconfig.lua
@@ -5,6 +5,8 @@ local capabilities = require"cmp_nvim_lsp".default_capabilities()
 -- local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities.textDocument.completion.completionItem.snippetSupport = true
 
+-- lspconfig['ast_grep'].setup{ capabilities = capabilities }
+
 lspconfig['emmet_ls'].setup{
     capabilities = capabilities,
     filetypes = { "html" }
