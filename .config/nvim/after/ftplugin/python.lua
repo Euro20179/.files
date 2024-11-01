2f4e87c6e29e3eafa6376fe4c8b135dcec69efc9
euro20179 <sandollar2020@gmail.com>
2024-11-01 14:22:21 -0700

    nvim: only use jumping nodes for markdown

--- .config/nvim/after/ftplugin/python.lua
+++ .config/nvim/after/ftplugin/python.lua
@@ -1,21 +1,21 @@
-vim.keymap.set("i", "<c-.>", function()
-    JumpToNearNodes(1, {
-        parameters = {
-            child_name = "identifier",
-        },
-        list = {
-            child_name = "^[a-z]*$",
-        }
-    })
-end)
-
-vim.keymap.set("i", "<c-s-.>", function()
-    JumpToNearNodes(-1, {
-        parameters = {
-            child_name = "identifier",
-        },
-        list = {
-            child_name = "^[a-z]*$",
-        }
-    })
-end)
+-- vim.keymap.set("i", "<c-.>", function()
+--     JumpToNearNodes(1, {
+--         parameters = {
+--             child_name = "identifier",
+--         },
+--         list = {
+--             child_name = "^[a-z]*$",
+--         }
+--     })
+-- end)
+--
+-- vim.keymap.set("i", "<c-s-.>", function()
+--     JumpToNearNodes(-1, {
+--         parameters = {
+--             child_name = "identifier",
+--         },
+--         list = {
+--             child_name = "^[a-z]*$",
+--         }
+--     })
+-- end)
