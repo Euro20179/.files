2935036d7a8700c6744fd479b98cbdcb00746373
euro20179 <sandollar2020@gmail.com>
2024-11-22 15:39:23 -0800

    docs: change readme

--- .config/nvim/lua/user/filetypes.lua
+++ .config/nvim/lua/user/filetypes.lua
@@ -19,35 +19,49 @@ vim.filetype.add({
                 if line1 == nil then
                     return
                 end
+
+                --nvim lua {{{
                 if vim.endswith(line1, "nvim -l") then
                     return "lua"
                 end
+                --}}}
+
+                --nvim -S {{{
                 if vim.endswith(line1, "nvim -S") then
                     if vim.endswith(path, ".lua") then
                         return "lua"
                     end
                     return "vim"
                 end
+                --}}}
+
+                --pacman {{{
                 local pkgData = vim.split(line1, " ", {})
                 if #pkgData == 1 then
                     return
                 end
+
                 local o = vim.system({"pacman", "-Qi", pkgData[1]}):wait()
                 if o.code == 1 then
                     return
                 end
+
                 local stdout = o.stdout
                 if stdout == nil then
                     return
                 end
+
                 local lines = vim.split(stdout, "\n")
+
                 local versionLine = lines[2]
                 local version = vim.split(versionLine, " : ")[2]
+
                 if version == pkgData[2] then
                     return 'pacman-package-list'
                 else
                     return
                 end
+                --}}}
             end
         }
     }
