a6f810422d86d0c463c406431951d0ba7d019f87
euro20179 <sandollar2020@gmail.com>
2024-12-15 12:09:08 -0800

    alias: better filetype for Qalc nvim

--- .config/nvim/queries/mmfml/injections.scm
+++ .config/nvim/queries/mmfml/injections.scm
@@ -2,3 +2,8 @@
    (language) @injection.language
    (code_text) @injection.content
    ))
+
+((inline_code . (code) @injection.content) (#set! injection.language "amml"))
+((inline_code
+   (language) @injection.language
+   (code) @injection.content))
