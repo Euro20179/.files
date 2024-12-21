7229cb78d9b0d82859e209848ddd398cffd431e8
euro20179 <euro20179@protonmail.com>
2024-12-20 16:34:43 -0800

    nvim: make ui.open normalize paths better

--- .config/nvim/lua/user/globals-setup.lua
+++ .config/nvim/lua/user/globals-setup.lua
@@ -3,8 +3,36 @@ vim.fn.setenv("NVIM", vim.v.servername)
 vim.diagnostic.config({ virtual_text = true, signs = true })
 
 ---@diagnostic disable-next-line
-vim.ui.open = function(item)
-    vim.system({ "linkhandler", item })
+vim.ui.open = function(item, opts)
+    local expanded = vim.fn.expand(item)
+    local fullPath = ""
+
+    --if url
+    if item:find("^%w+://") then
+        fullPath = item
+    --if absolute
+    elseif vim.startswith(expanded, "~") or
+        vim.startswith(expanded, "/")
+    then
+        fullPath = expanded
+    --otherwise normalize
+    else
+        -- this should not be the default way to merge paths lmao
+        fullPath = vim.fs.normalize(
+                vim.fn.fnamemodify(vim.fn.expand("%:p:h"), ":p")
+                ..
+                vim.fn.expand(item)
+        )
+    end
+
+    local cmd = {"linkhandler"}
+    if opts.cmd ~= nil then
+        ---@type string[]
+        cmd = opts.cmd
+    end
+
+    cmd[#cmd+1] = fullPath
+    vim.system(cmd)
 end
 
 vim.ui.select = require "mini.pick".ui_select
