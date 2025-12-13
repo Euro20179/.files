984d20c18d3bdc0bcafd2252602b170d7cc93e9e
euro20179 <sandollar2020@gmail.com>
2025-12-12 17:28:29 -0800

    nvim: update treesitter, and also make d* navigation shortcuts consistent

--- .config/nvim/after/plugin/treesitter.lua
+++ .config/nvim/after/plugin/treesitter.lua
@@ -1,153 +1,183 @@
-local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
-
-parser_config.hypr = {
-    install_info = {
-        url = "https://github.com/luckasRanarison/tree-sitter-hyprlang",
-        files = { "src/parser.c" },
-        branch = "master",
-    },
-    filetype = "hypr",
-}
-
-parser_config.mmfml = {
-    install_info = {
-        url = "/home/euro/Programs/Coding Projects/treesitter/mmfml/",
-        files = { "src/parser.c", "src/scanner.c" },
-        generate_requires_npm = false,
-        requires_generate_from_grammar = true,
-    },
-    filetype = "mmfml"
-}
-
-parser_config.amml = {
-    install_info = {
-        url = "/home/euro/Programs/Coding Projects/treesitter/amml",
-        files = { "src/parser.c" },
-        branch = "master",
-        generate_requires_npm = false,
-        requires_generate_from_grammar = false
-    }
-}
-
-parser_config.bircle = {
-    install_info = {
-        url = "/home/euro/Programs/Coding Projects/treesitter/bircle",
-        files = { "src/parser.c" },
-        branch = "main",
-        generate_requires_npm = false,
-        requires_generate_from_grammar = false,
-    },
-    filetype = "bircle"
-}
-
-require 'nvim-treesitter.configs'.setup {
-    ensure_installed = {"c", "lua", "vim", "vimdoc", "query"},
-    sync_install = false,
-    auto_install = false,
-    ignore_install = {},
-
-    modules = {},
-
-    highlight = {
-        enable = true,
-        -- additional_vim_regex_highlighting = { "org", "vim"}
-    },
-
-    playground = {
-        enable = true,
-        keybinds = {
-            toggle_query_editor = "o",
-            toggle_hl_groups = "i",
-            toggle_injected_languages = "t",
-            toggle_anonymous_nodes = "a",
-            toggle_language_display = "I",
-            focus_language = "f",
-            unfocus_language = "F",
-            update = "R",
-            goto_node = "<cr>",
-            show_help = "?"
+vim.api.nvim_create_autocmd("User", { pattern = "TSUpdate",
+    callback = function()
+        require"nvim-treesitter.parsers".mmfml = {
+            install_info = {
+                url = "https://github.com/euro20179/treesitter-mmfml",
+                revision = "master",
+            }
         }
-    },
-
-    indent = {
-        enable = true
-    },
-
-    incremental_selection = {
-        enable = true,
-        keymaps = {
-            init_selection = "<leader>vn",
-            node_incremental = 'gin',
-            scope_incremental = "gis",
-            node_decremental = "giN",
+        require"nvim-treesitter.parsers".amml = {
+            install_info = {
+                url = "https://github.com/euro20179/treesitter-amml",
+                revision = "master",
+            }
         }
-    },
+    end
+})
 
-    textobjects = {
-        swap = {
-            enable = true,
-            swap_next = {
-                ["<A-n>"] = "@parameter.inner",
-            },
+vim.api.nvim_create_autocmd("FileType", {
+    callback = function()
+        local lang = vim.treesitter.language.get_lang(vim.bo[0].filetype)
+        if lang == "mmfml" or lang == "amml" or require"nvim-treesitter.parsers"[lang] ~= nil then
+            local _, ok = pcall(vim.treesitter.start, 0, lang)
+        end
+    end
+})
 
-            swap_previous = {
-                ["<A-p>"] = "@parameter.inner",
-            },
-        },
+vim.treesitter.language.register("mmfml", { "mmfml" })
+vim.treesitter.language.register("amml", { "amml" })
 
-        move = {
-            enable = true,
-            set_jumps = true, -- whether to set jumps in the jumplist
-            goto_next_start = {
-                ["]f"] = "@function.outer",
-                ["]]"] = { query = "@class.outer", desc = "Next class start" },
-                ["]%"] = "@block.outer"
-            },
 
-            goto_next_end = {
-                ["]F"] = "@function.outer",
-                ["]["] = "@class.outer",
-            },
-
-            goto_previous_start = {
-                ["[f"] = "@function.outer",
-                ["[["] = "@class.outer",
-                ["[%"] = "@block.outer"
-            },
-
-            goto_previous_end = {
-                ["[F"] = "@function.outer",
-                ["[]"] = "@class.outer",
-            },
-        },
-
-        select = {
-            enable = true,
-            keymaps = {
-                ["af"] = "@function.outer",
-                ["if"] = "@function.inner",
-                ["ac"] = "@class.outer",
-                ["ic"] = "@class.inner",
-                ["ai"] = "@conditional.outer",
-                ["ii"] = "@conditional.inner",
-                ["al"] = "@loop.outer",
-                ["il"] = "@loop.inner",
-                ["i%"] = "@block.inner",
-                ["a%"] = "@block.outer",
-                ["ia"] = "@parameter.inner",
-                ["aa"] = "@parameter.outer",
-                ["i$"] = "@call.inner",
-                ["a$"] = "@call.outer",
-                ["i="] = "@assignment.inner",
-                ["ik"] = "@assignment.lhs",
-                ["a="] = "@assignment.outer",
-                ["iv"] = "@assignment.rhs",
-                ["ir"] = "@regex.inner",
-                ['ar'] = "@regex.outer"
-            },
-        },
-    }
-}
+-- local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+--
+-- parser_config.hypr = {
+--     install_info = {
+--         url = "https://github.com/luckasRanarison/tree-sitter-hyprlang",
+--         files = { "src/parser.c" },
+--         branch = "master",
+--     },
+--     filetype = "hypr",
+-- }
+--
+-- parser_config.mmfml = {
+--     install_info = {
+--         url = "/home/euro/Programs/Coding Projects/treesitter/mmfml/",
+--         files = { "src/parser.c", "src/scanner.c" },
+--         generate_requires_npm = false,
+--         requires_generate_from_grammar = true,
+--     },
+--     filetype = "mmfml"
+-- }
+--
+-- parser_config.amml = {
+--     install_info = {
+--         url = "/home/euro/Programs/Coding Projects/treesitter/amml",
+--         files = { "src/parser.c" },
+--         branch = "master",
+--         generate_requires_npm = false,
+--         requires_generate_from_grammar = false
+--     }
+-- }
+--
+-- parser_config.bircle = {
+--     install_info = {
+--         url = "/home/euro/Programs/Coding Projects/treesitter/bircle",
+--         files = { "src/parser.c" },
+--         branch = "main",
+--         generate_requires_npm = false,
+--         requires_generate_from_grammar = false,
+--     },
+--     filetype = "bircle"
+-- }
+--
+-- require 'nvim-treesitter.configs'.setup {
+--     ensure_installed = {"c", "lua", "vim", "vimdoc", "query"},
+--     sync_install = false,
+--     auto_install = false,
+--     ignore_install = {},
+--
+--     modules = {},
+--
+--     highlight = {
+--         enable = true,
+--         -- additional_vim_regex_highlighting = { "org", "vim"}
+--     },
+--
+--     playground = {
+--         enable = true,
+--         keybinds = {
+--             toggle_query_editor = "o",
+--             toggle_hl_groups = "i",
+--             toggle_injected_languages = "t",
+--             toggle_anonymous_nodes = "a",
+--             toggle_language_display = "I",
+--             focus_language = "f",
+--             unfocus_language = "F",
+--             update = "R",
+--             goto_node = "<cr>",
+--             show_help = "?"
+--         }
+--     },
+--
+--     indent = {
+--         enable = true
+--     },
+--
+--     incremental_selection = {
+--         enable = true,
+--         keymaps = {
+--             init_selection = "<leader>vn",
+--             node_incremental = 'gin',
+--             scope_incremental = "gis",
+--             node_decremental = "giN",
+--         }
+--     },
+--
+--     textobjects = {
+--         swap = {
+--             enable = true,
+--             swap_next = {
+--                 ["<A-n>"] = "@parameter.inner",
+--             },
+--
+--             swap_previous = {
+--                 ["<A-p>"] = "@parameter.inner",
+--             },
+--         },
+--
+--         move = {
+--             enable = true,
+--             set_jumps = true, -- whether to set jumps in the jumplist
+--             goto_next_start = {
+--                 ["]f"] = "@function.outer",
+--                 ["]]"] = { query = "@class.outer", desc = "Next class start" },
+--                 ["]%"] = "@block.outer"
+--             },
+--
+--             goto_next_end = {
+--                 ["]F"] = "@function.outer",
+--                 ["]["] = "@class.outer",
+--             },
+--
+--             goto_previous_start = {
+--                 ["[f"] = "@function.outer",
+--                 ["[["] = "@class.outer",
+--                 ["[%"] = "@block.outer"
+--             },
+--
+--             goto_previous_end = {
+--                 ["[F"] = "@function.outer",
+--                 ["[]"] = "@class.outer",
+--             },
+--         },
+--
+--         select = {
+--             enable = true,
+--             keymaps = {
+--                 ["af"] = "@function.outer",
+--                 ["if"] = "@function.inner",
+--                 ["ac"] = "@class.outer",
+--                 ["ic"] = "@class.inner",
+--                 ["ai"] = "@conditional.outer",
+--                 ["ii"] = "@conditional.inner",
+--                 ["al"] = "@loop.outer",
+--                 ["il"] = "@loop.inner",
+--                 ["i%"] = "@block.inner",
+--                 ["a%"] = "@block.outer",
+--                 ["ia"] = "@parameter.inner",
+--                 ["aa"] = "@parameter.outer",
+--                 ["i$"] = "@call.inner",
+--                 ["a$"] = "@call.outer",
+--                 ["i="] = "@assignment.inner",
+--                 ["ik"] = "@assignment.lhs",
+--                 ["a="] = "@assignment.outer",
+--                 ["iv"] = "@assignment.rhs",
+--                 ["ir"] = "@regex.inner",
+--                 ['ar'] = "@regex.outer"
+--             },
+--         },
+--     }
+-- }
 --
 -- require'treesitter-context'.setup{
 --     enable = false, -- Enable this plugin (Can be enabled/disabled later via commands)
--- .config/nvim/lua/user/plugins2.lua
+++ .config/nvim/lua/user/plugins2.lua
@@ -36,8 +36,8 @@ pkg.add({
 
 -- Treesitter{{{
 pkg.add({
-    { src = "https://github.com/nvim-treesitter/nvim-treesitter" },
-    { src = "https://github.com/nvim-treesitter/nvim-treesitter-textobjects" }
+    { src = "https://github.com/nvim-treesitter/nvim-treesitter", rev = "main", branches = { "main" }, version = "main" },
+    -- { src = "https://github.com/nvim-treesitter/nvim-treesitter-textobjects" }
 }, "now")
 -- }}}
 
@@ -217,6 +217,21 @@ pkg.add({ { src = "https://github.com/stevearc/oil.nvim" } }, "now", {
                 show_hidden = true,
             },
             keymaps = {
+                ds = function ()
+                    vim.cmd[[e oil://$XDG_DOWNLOAD_HOME]]
+                end,
+                dF = function()
+                    vim.cmd[[e oil:///$XDG_DOCUMENTS_HOME]]
+                end,
+                dcg = function()
+                    vim.cmd[[e oil://$CLOUD/Games]]
+                end,
+                dc = function()
+                    vim.cmd[[e oil://$CLOUD]]
+                end,
+                dm = function()
+                    vim.cmd[[e oil://$XDG_PICTURES_HOME/memes]]
+                end,
                 yc = function()
                     local dir = require "oil".get_current_dir()
                     vim.fn.setreg("\"", dir)
