fb764671151796936fc021cdc91c38109888d65a
euro20179 <Euro20171@proton.me>
2025-12-19 08:43:15 -0800

    nvim: use sist2 to make a wiki search

--- .config/nvim/plugin/last-modified.vim
+++ .config/nvim/plugin/last-modified.vim
@@ -1,15 +1,10 @@
 " Author: euro
-" Last Modified 12/19/2025 07:39
+" Last Modified 12/19/2025 08:18
 " Description: If the file contains the text `Last Modified: <date> <time>` it
 " will replace that date and time with the current time on :w
 function s:handleLastModified()
     let pos = getpos(".")
-    try
-        undojoin
-        "no :undojoin after `u`
-    catch /E790:/
-        return
-    endtry
+    undojoin
 
     let count = search('\<Last Modified:\?\_$\?\n\?\_^\?\s*[[:digit:]]\{2\}\/[[:digit:]]\{2\}\/[[:digit:]]\{4\} [[:digit:]]\{2\}:[[:digit:]]\{2\}\>', "p")
     if count == 0 | return | endif
--- .config/nvim/plugin/vimwiki.lua
+++ .config/nvim/plugin/vimwiki.lua
@@ -1,5 +1,5 @@
 --[=[
-Last Modified 12/19/2025 07:41
+Last Modified 12/19/2025 08:42
 Description: Whatever improvements I feel like adding to my vimwiki
 --]=]
 local aug = vim.api.nvim_create_augroup("conf.vimwiki", {})
@@ -21,6 +21,64 @@ vim.api.nvim_create_autocmd("BufNewFile", {
         ]]
     end
 })
+
+local sist_process
+
+vim.api.nvim_create_autocmd("VimLeave", {
+    group = aug,
+    callback = function ()
+        if sist_process ~= nil then
+            sist_process:kill()
+        end
+    end
+})
+
+vim.api.nvim_create_autocmd("VimEnter", {
+    group = aug,
+    once = true,
+    pattern = vim.fn.expand("$WIKI") .. "/*",
+    callback = function()
+        sist_process = vim.system({"sist2", "web", "--search-index", vim.fn.expand("$WIKI/.sist2/index.sist2"), vim.fn.expand("$WIKI/.sist2/scan.sist2")})
+
+        vim.keymap.set("n", "<c-s>", ":Search ")
+
+        vim.api.nvim_create_user_command("Search", function(data)
+            local search = data.args
+            local post_data = {
+                query = search,
+                sort = "score",
+                highlight = true,
+                pageSize = 1000,
+                indexIds = {1766161970},
+            }
+
+            local res = vim.system({"curl", "-X", "POST", "-d", vim.json.encode(post_data), "http://127.0.0.1:4090/fts/search"}):wait()
+            if res.code ~= 0 then
+                vim.notify("Failed to make query", vim.log.levels.ERROR)
+                return
+            end
+
+            local res = vim.json.decode(res.stdout)
+
+            local loclist = {}
+
+            local wiki_root = vim.fn.expand("$WIKI")
+
+            for _, hit in pairs(res["hits"]["hits"]) do
+                loclist[#loclist+1] =  {
+                    filename = wiki_root .. '/' .. hit._source.path .. '/' .. hit._source.name .. '.' .. hit._source.extension,
+                    lnum = 1,
+                    text = string.gsub(hit.highlight.name, "</?mark>", "")
+                }
+            end
+
+            vim.fn.setloclist(0, loclist)
+            vim.cmd[[lwin]]
+        end, {nargs = "*" })
+    end
+})
+
+
 --
 -- vim.api.nvim_create_autocmd("VimEnter", {
 --     callback = function()
